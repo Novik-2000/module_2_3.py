586052d519e916407f47388203e1b127bd7feb59
Novik-2000 <lyubasha.novikova.2000@mail.ru>
2024-09-17 22:52:29 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,44 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+neutral_number = 0
+print(len(my_list))
+while True:
+    if my_list[0] > 0:
+        print(my_list[0])
+    elif my_list[0] < 0:
+        break
+    if my_list [1] > 0 :
+        print(my_list[1])
+    elif my_list[1] < 0:
+        break
+    if my_list[2] > 0:
+        print(my_list[2])
+    elif my_list[2] < 0:
+        break
+    if my_list[3] > 0:
+        print(my_list[3])
+    elif my_list[3] < 0:
+        break
+    if my_list [4] > 0:
+        print(my_list[4])
+    elif my_list [4] <0:
+        break
+    if my_list[5] > 0:
+        print(my_list[5])
+    elif my_list[5] < 0:
+        break
+    if my_list[6] > 0:
+        print(my_list[6])
+    elif my_list[6] < 0:
+        break
+    if my_list[7] > 0:
+        print(my_list[7])
+    elif my_list[7] < 0:
+        break
+    if my_list [8] > 0 :
+        print(my_list[8])
+    elif my_list[8] < 0:
+        break
+    if my_list[9] > 0:
+        print(my_list[9])
+    elif my_list[9] < 0:
+        break
