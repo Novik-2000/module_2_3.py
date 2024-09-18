20d924665f9e24ce180f970052e54654bc81831a
Novik-2000 <lyubasha.novikova.2000@mail.ru>
2024-09-18 21:18:25 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,44 +1,13 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-neutral_number = 0
+n = 0
 print(len(my_list))
+
+
 while True:
-    if my_list[0] > 0:
-        print(my_list[0])
-    elif my_list[0] < 0:
+    if my_list[n] > 0:
+        print(my_list[n])
+        n += 1
+        continue
+    elif my_list[n] < 0:
         break
-    if my_list [1] > 0 :
-        print(my_list[1])
-    elif my_list[1] < 0:
-        break
-    if my_list[2] > 0:
-        print(my_list[2])
-    elif my_list[2] < 0:
-        break
-    if my_list[3] > 0:
-        print(my_list[3])
-    elif my_list[3] < 0:
-        break
-    if my_list [4] > 0:
-        print(my_list[4])
-    elif my_list [4] <0:
-        break
-    if my_list[5] > 0:
-        print(my_list[5])
-    elif my_list[5] < 0:
-        break
-    if my_list[6] > 0:
-        print(my_list[6])
-    elif my_list[6] < 0:
-        break
-    if my_list[7] > 0:
-        print(my_list[7])
-    elif my_list[7] < 0:
-        break
-    if my_list [8] > 0 :
-        print(my_list[8])
-    elif my_list[8] < 0:
-        break
-    if my_list[9] > 0:
-        print(my_list[9])
-    elif my_list[9] < 0:
-        break
+    n += 1
